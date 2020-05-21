a089ee43a970ec088d2028eb4495b48ea2a0fec4
liuzhuoling <zoran.liu@amberaigroup.com>
2020-05-21 14:30:38 +0800

    fix: update UI

--- utils/version.go
+++ utils/version.go
@@ -1,11 +1,11 @@
 package utils
 
 var (
-	name = "RedisGo"
-	version = "2.0.3"
-	author = "zoranjojo"
-	email = "liuzhuoling2011@hotmail.com"
-	desc = "RedisGo Web是为更优雅的监控/管理内网的Redis而倾心打造"
+	name    = "RedisGo"
+	version = "2.0.4"
+	author  = "zoranjojo"
+	email   = "liuzhuoling2011@hotmail.com"
+	desc    = "RedisGo Web是为更优雅的监控/管理内网的Redis而倾心打造"
 )
 
 func GetName() string { return name }
@@ -17,4 +17,3 @@ func GetAuthor() string { return author }
 func GetEmail() string { return email }
 
 func GetDescription() string { return desc }
-
